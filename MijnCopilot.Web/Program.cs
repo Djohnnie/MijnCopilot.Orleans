f0ae1d34066c6f89a150dc8e4dacf84de1557b4b
Djohnnie <7031043+Djohnnie@users.noreply.github.com>
2026-03-08 18:10:09 +0100

    Added Auth0 integration

--- MijnCopilot.Web/Program.cs
+++ MijnCopilot.Web/Program.cs
@@ -1,16 +1,35 @@
-﻿using MijnCopilot.Application.DependencyInjection;
-using MijnCopilot.Agents.DependencyInjection;
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using MijnCopilot.Application.DependencyInjection;
 using MijnCopilot.Web.Components;
 using MijnCopilot.Web.Helpers;
 using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuth0WebAppAuthentication(options =>
+{
+    options.Domain = builder.Configuration.GetValue<string>("AUTH0_DOMAIN")!;
+    options.ClientId = builder.Configuration.GetValue<string>("AUTH0_CLIENTID")!;
+    options.ClientSecret = builder.Configuration.GetValue<string>("AUTH0_CLIENTSECRET");
+});
+
+builder.Services.Configure<CookieAuthenticationOptions>(
+    CookieAuthenticationDefaults.AuthenticationScheme,
+    options =>
+    {
+        options.LoginPath = "/Login";
+        options.LogoutPath = "/Logout";
+    });
+
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddScoped<NavigationHelper>();
 builder.Services.AddMudServices();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -20,11 +39,15 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapRazorPages();
 
 app.Run();
